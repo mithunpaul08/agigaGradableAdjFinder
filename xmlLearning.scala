228ba998d374f8920e7283af5122b10ed71fe10d
mithunpaul08 <mithunpaul08@gmail.com>
2016-09-30 01:45:17 -0700

    testing for cached credentials

--- xmlLearning.scala
+++ xmlLearning.scala
@@ -7,7 +7,7 @@ object Hi {
 	val testFile  = XML.loadFile("testXml.xml")
 //	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
 	val hello= (testFile \\ "POS").map(_.text)
-
+//just sample edit
 agigParser.readFiles()
 agigParser.printLine()
 
