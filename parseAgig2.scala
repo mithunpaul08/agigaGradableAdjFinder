0ecce9c556f5e16cb6ed3c16cb2a348a4b6a22ac
mithunpaul08 <mithunpaul08@gmail.com>
2016-10-05 22:06:14 -0700

    replaced the code with the latest code from jenny that recognizes grad adjectives

--- parseAgig2.scala
+++ parseAgig2.scala
@@ -5,18 +5,40 @@ import org.clulab.processors.Document
 import java.io.File
 import scala.collection.parallel.ForkJoinTaskSupport
 import java.io._
+import java.util.Arrays
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+
+//import org.apache.commons.io.FileUtils;
+//import org.apache.commons.io.comparator.SizeFileComparator;
 
 
 object agigParser{
-val completeAgigaFiles = "net/kate/storage/data/nlp/corpora/agiga/data/xml/"
+val baseDirectoryPath = "/net/kate/storage/data/nlp/corpora/agiga/data/xml/"
 //val baseDirectoryPath = "/work/mithunpaul/gzips/"
-val baseDirectoryPath = "/work/mithunpaul/gzipsJustOne/"
+//val baseDirectoryPath = "/work/mithunpaul/gzipsJustOne/"
 val outputDirectoryPath = "/work/mithunpaul/outputs/"
 // the xml files are here
 val files = new File(baseDirectoryPath).listFiles.par
-val nthreads = 2
+
+
+//val files = new File(baseDirectoryPath).listFiles
+//val filesRaw = new File(baseDirectoryPath).listFiles
+//val files = filesRaw.sorted(Ordering.by(File.size).reverse)
+//val files = filesRaw.sorted
+//val files = filesRaw.sorted(Ordering.Any.reverse)
+//val files = filesRaw.sorted(Ordering.by(File.getTotalSpace()))
+//val files = filesRaw.sortBy(_.size)
+//sortWith(sortByLength)
+//scala.util.Sorting.quickSort(files)
+//filesRaw.sortWith(sortByLength)
+
+//Arrays.sort(files, SizeFileComparator.SIZE_COMPARATOR)
+val nthreads = 8
 // limit parallelization
-files.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(nthreads))
+//files.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(nthreads))
 
 /** extract words tagged as adjectives */
 def processDocument(doc: Document): Array[String] = for {
@@ -43,21 +65,38 @@ println("finished writing adjectives to file")
 
 def readFiles (): Unit =
 {
-	for (f <- files) {
+	println("reaching here at 1")
+
+
+	//run a random number generator inside an always true for loop
+	//for(1){
+	while(true)
+	{
+	val rand= scala.util.Random
+	val i= rand.nextInt(1000)
+	//for ( i <-1 to 10){
+	val f=files(i)
+	//for (f <- files) {
 	val fileName=f.getName
-	println("starting identification of adjectives in agigafile:" +fileName)
+	println("reaching here at 27")
 	val outFile = new File(outputDirectoryPath, "gradAdj"+ fileName+".txt")
     	// make sure the file hasn't been already processed
     	// useful when restarting
+	println("reaching here at 2")
 	if (!outFile.exists)
     	{
+	println("starting identification of adjectives in agigafile:" +fileName)
+	println("reaching here at 3")
 	val doc = agiga.toDocument(f.getAbsolutePath)
     	println("finished finding adjectives in"+fileName)
     	val adjectives = processDocument(doc)
-     	 val uniqAdj = adjectives.distinct
+	println("reaching here at 4")
+     	// val uniqAdj = adjectives.distinct
     	//write to file
+	println("reaching here at 5")
 	val bw = new BufferedWriter(new FileWriter(outFile))
-	bw.write(uniqAdj.mkString("\n "))
+	bw.write(adjectives.mkString("\n "))
+	println("reaching here at 6")
 	bw.close()
  	}
 }
--- xmlLearning.scala
+++ xmlLearning.scala
@@ -5,11 +5,16 @@ import scala.xml.XML
 object Hi {
  def main(args: Array[String]) = println("Hi!")
 	val testFile  = XML.loadFile("testXml.xml")
+println("reaching here at 21")
 //	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
 	val hello= (testFile \\ "POS").map(_.text)
 //just sample edit
+println("reaching here at 22")
+//	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
 agigParser.readFiles()
+println("reaching here at 23")
 agigParser.printLine()
+println("reaching here at 24")
 
 
 println(hello)
