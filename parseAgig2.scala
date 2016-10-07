a3c2c1189c4bf85ca63714cd25121c8466a4fa1e
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2016-10-06 20:48:42 -0700

    compiles, but gives a runtime error. Pushing to test on jenny

--- parseAgig2.scala
+++ parseAgig2.scala
@@ -15,106 +15,156 @@ import java.util.Comparator;
 //import org.apache.commons.io.comparator.SizeFileComparator;
 
 
-object agigParser{
-val baseDirectoryPath = "/net/kate/storage/data/nlp/corpora/agiga/data/xml/"
-//val baseDirectoryPath = "/work/mithunpaul/gzips/"
-//val baseDirectoryPath = "/work/mithunpaul/gzipsJustOne/"
-val outputDirectoryPath = "/work/mithunpaul/outputs/"
-// the xml files are here
-val files = new File(baseDirectoryPath).listFiles.par
+object agigParser {
+	//val baseDirectoryPath = "/net/kate/storage/data/nlp/corpora/agiga/data/xml/"
+	/* path in local machine */
+	val baseDirectoryPath = "/home/mithunpaul/Desktop/fall2016NLPResearch/agigaParser-without-world-modeling/inputs/"
+
+	//a relative path, instead of absolute path
+	//val baseDirectoryPath = "/inputs/"
+	//val baseDirectoryPath = "/work/mithunpaul/gzipsJustOne/"
+	//val outputDirectoryPath = "/work/mithunpaul/outputs/"
+	val outputDirectoryPath = "./outputs/"
+	// the xml files are here
+	//val files = new File(baseDirectoryPath).listFiles.par
+
+	/*dont parallelize it if its a single core machine*/
+	val files = new File(baseDirectoryPath).listFiles
 
 
-//val files = new File(baseDirectoryPath).listFiles
-//val filesRaw = new File(baseDirectoryPath).listFiles
-//val files = filesRaw.sorted(Ordering.by(File.size).reverse)
-//val files = filesRaw.sorted
-//val files = filesRaw.sorted(Ordering.Any.reverse)
-//val files = filesRaw.sorted(Ordering.by(File.getTotalSpace()))
-//val files = filesRaw.sortBy(_.size)
-//sortWith(sortByLength)
-//scala.util.Sorting.quickSort(files)
-//filesRaw.sortWith(sortByLength)
 
-//Arrays.sort(files, SizeFileComparator.SIZE_COMPARATOR)
-val nthreads = 8
-// limit parallelization
-//files.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(nthreads))
-var adjCounter = 2;
-var lemma="test"
+	//val files = new File(baseDirectoryPath).listFiles
+	//val filesRaw = new File(baseDirectoryPath).listFiles
+	//val files = filesRaw.sorted(Ordering.by(File.size).reverse)
+	//val files = filesRaw.sorted
+	//val files = filesRaw.sorted(Ordering.Any.reverse)
+	//val files = filesRaw.sorted(Ordering.by(File.getTotalSpace()))
+	//val files = filesRaw.sortBy(_.size)
+	//sortWith(sortByLength)
+	//scala.util.Sorting.quickSort(files)
+	//filesRaw.sortWith(sortByLength)
 
-/** extract words tagged as adjectives */
-def processDocument(doc: Document): Array[String] =
- { 
-//for {
- // s <- doc.sentences
- // (t, i) <- s.tags.get.zipWithIndex
-  // is it an adjective?i
-var mystring="this is a string"
- // if (mystring.t.startsWith("JJ"))
-  if (mystring.startsWith("this"))
-	{
-	println("the string you gave starts with this inside world")
-	
-	//var w = s.words(i)
+	//Arrays.sort(files, SizeFileComparator.SIZE_COMPARATOR)
+	val nthreads = 8
+	// limit parallelization
+	//files.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(nthreads))
+	var adjCounter = 2
+	//var lemma="test"
 
-	//lemma = s.lemmas.get(i)
-	/adjCounter =adjCounter + 1;
-	//println("value of adjCounter is"+adjCounter);
-	//to find if the adjective ends with -est or -er
-	//if w.matches(".*(est|er)$")
-	  // get the lemma
-	// lemma = s.lemmas.get(i)
-}
-//}	
+	/** extract words tagged as adjectives */
+	def processDocument(doc: Document): String = {
+		var mystring = "this is a string"
+		var lemma = "teststring"
+		for (s <- doc.sentences) {
+			for ((t, i) <- s.tags.get.zipWithIndex) {
+				println("inside the doc.sentences for loop. The sentence is " + s)
+
+
+				//for {
+				// s <- doc.sentences
+				// (t, i) <- s.tags.get.zipWithIndex
+				// is it an adjective?i
+				//if (mystring.startsWith("this")) {
+				if (t.startsWith("JJ"))
+					{
+
+					println("the string you gave starts with this inside world")
+
+					var w = s.words(i)
+
+
+					//lemma = s.lemmas.get(i)
+					adjCounter = adjCounter + 1;
+
+					println("value of adjCounter is" + adjCounter);
+					//to find if the adjective ends with -est or -er
+					if (w.matches(".*(est|er)$"))
+					{
+						// get the lemma
+						lemma = s.lemmas.get(i)
+					}
+				}
+			}
+		}
+		return lemma;
+	}
+
+
+	/*
+def processDocument(doc: Document): Array[String] = {
+	//for {
+	// s <- doc.sentences
+	// (t, i) <- s.tags.get.zipWithIndex
+	// is it an adjective?i
+	var mystring = "this is a string"
+	// if (mystring.t.startsWith("JJ"))
+	if (mystring.startsWith("this")) {
+		println("the string you gave starts with this inside world")
+
+		//var w = s.words(i)
+
+		var lemma ="teststring"
+		//lemma = s.lemmas.get(i)
+		/ adjCounter = adjCounter + 1;
+		//println("value of adjCounter is"+adjCounter);
+		//to find if the adjective ends with -est or -er
+		//if w.matches(".*(est|er)$")
+		// get the lemma
+		// lemma = s.lemmas.get(i)
+	}
+	//}
 } yield lemma
+*/
 
 
+	def printLine(): Unit = {
+		println("finished writing adjectives to file")
+	}
+
+	def readFiles(): Unit = {
+		println("reaching here at 1")
+
+		//def getCurrentDirectory = new java.io.File(".").getCanonicalPath
+		var getCurrentDirectory = new java.io.File(".").getCanonicalPath
+		println("value of present directory is: "+getCurrentDirectory)
 
 
-def printLine (): Unit =
-{
-println("finished writing adjectives to file")
-}
-
-def readFiles (): Unit =
-{
-	println("reaching here at 1")
-
-
-	//run a random number generator inside an always true for loop
-	//for(1){
-	while(true)
-	{
-	val rand= scala.util.Random
-	val i= rand.nextInt(1000)
-	//for ( i <-1 to 10){
-	val f=files(i)
-	//for (f <- files) {
-	val fileName=f.getName
-	println("reaching here at 27")
-	val outFile = new File(outputDirectoryPath, "gradAdj"+ fileName+".txt")
-    	// make sure the file hasn't been already processed
-    	// useful when restarting
-	println("reaching here at 2")
-	if (!outFile.exists)
-    	{
-	println("starting identification of adjectives in agigafile:" +fileName)
-	println("reaching here at 3")
-	val doc = agiga.toDocument(f.getAbsolutePath)
-    	println("finished finding adjectives in"+fileName)
-    	val adjectives = processDocument(doc)
-	println("reaching here at 4")
-     	// val uniqAdj = adjectives.distinct
-    	//write to file
-	println("reaching here at 5")
-	val bw = new BufferedWriter(new FileWriter(outFile))
-	bw.write(adjectives.mkString("\n "))
-	println("value of adjcounter is ")
-println(adjCounter)
-	bw.close()
- 	}
-}
- }
+		//run a random number generator inside an always true for loop- to pick files randomly and parse
+		//for(1){
+//
+//		while (true) {
+//			val rand = scala.util.Random
+//			val i = rand.nextInt(1000)
+//
+			//for ( i <-1 to 10){
+			//val f = files(i)
+			for (individualFile <- files) {
+			val fileName = individualFile.getName
+			println("reaching here at 27")
+			val outFile = new File(outputDirectoryPath, "gradAdj" + fileName + ".txt")
+			// make sure the file hasn't been already processed
+			// useful when restarting
+			println("reaching here at 2")
+			if (!outFile.exists) {
+				println("starting identification of adjectives in agigafile:" + fileName)
+				println("reaching here at 3")
+				println("the absolute path of the current document is: "+individualFile.getAbsolutePath)
+				val doc = agiga.toDocument(individualFile.getAbsolutePath)
+				//val doc = agiga.toDocument(fileName)
+				println("finished finding adjectives in" + fileName)
+				val adjectives = processDocument(doc)
+				println("reaching here at 4")
+				// val uniqAdj = adjectives.distinct
+				//write to file
+				println("reaching here at 5")
+				val bw = new BufferedWriter(new FileWriter(outFile))
+				bw.write(adjectives.mkString("\n "))
+				println("value of adjcounter is ")
+				println(adjCounter)
+				bw.close()
+			}
+		}
+	}
 
 
 }
--- xmlLearning.scala
+++ xmlLearning.scala
@@ -3,39 +3,39 @@ import scala.xml.XML
 //test
 
 object Hi {
- def main(args: Array[String]) = println("Hi!")
-	val testFile  = XML.loadFile("testXml.xml")
-println("reaching here at 21")
-//	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
-	val hello= (testFile \\ "POS").map(_.text)
-//just sample edit
-println("reaching here at 22")
-//	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
-agigParser.readFiles()
-println("reaching here at 23")
-var counter=1;
-var mystring ="this is an immutable string"
+	def main(args: Array[String]) = println("Hi!")
 
-println(mystring)
+	val testFile = XML.loadFile("./inputs/testXml.xml")
+	println("reaching here at 21")
+	//	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
+	val hello = (testFile \\ "POS").map(_.text)
+	//just sample edit
+	println("reaching here at 22")
+	//	val hello= (testFile \ "DOC" \ "sentences" \ "sentence" \ "tokens"  ).map(_.text)
+	agigParser.readFiles()
+	println("reaching here at 23")
+	var counter = 1;
+	var mystring = "this is an immutable string"
 
-var mynewstring = "this is an immutable string";
-var mylen=mynewstring.length()
+	println(mystring)
 
-println(mynewstring)
+	var mynewstring = "this is an immutable string";
+	var mylen = mynewstring.length()
 
-println("length of the string is"+mylen)
-println("value of counter is" + counter)
+	println(mynewstring)
 
-if (mynewstring.startsWith("this"))
-{
-println("given string starts with this")
-counter =counter +1
-}
-println("value of counter is" + counter)
-agigParser.printLine()
-println("reaching here at 24")
+	println("length of the string is" + mylen)
+	println("value of counter is" + counter)
+
+	if (mynewstring.startsWith("this")) {
+		println("given string starts with this")
+		counter = counter + 1
+	}
+	println("value of counter is" + counter)
+	agigParser.printLine()
+	println("reaching here at 24")
 
 
-println(hello)
+	println(hello)
 }
 
