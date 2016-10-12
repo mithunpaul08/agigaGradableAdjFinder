c055028e08eaad70a6fc4085ccc835c8337971a9
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2016-10-12 14:36:46 -0700

    finds the root form and adds it into a hashmap

--- compareErAdjWithUniq.scala
+++ compareErAdjWithUniq.scala
@@ -24,18 +24,22 @@ object goodAdjectiveFinder {
 3. if not, this is not an adjective. For example, "kilometer" or "silver" are not valid adjectives, because their base form, "kilomet" and "silv", will not appear in text.
 */
 
- //var resourcesDirectory = "/work/mithunpaul/newOutputs/allAdjectives/"
- var resourcesDirectory = "/work/mithunpaul/testbed/"
- // var resourcesDirectory = "resources/"
+
+  //var resourcesDirectory = "/work/mithunpaul/testbed/"
+  var resourcesDirectory = "resources/"
 
   var outputDirectoryPath = "outputs/"
 
-//	var completeAgigaFile="CompleteAgigaDump.txt";
-var completeAgigaFile="allAdjectivesFromAgigaButUniq.txt";
-  var erRemovedFiles="uniqAdjectivesInAgiga_removedErEst.txt";
+  //	var completeAgigaFile="CompleteAgigaDump.txt";
+  var completeAgigaFile = "allAdjectivesFromAgigaButUniq.txt";
+  //var erRemovedFiles="uniqAdjectivesInAgiga_removedErEst.txt";
+  var erRemovedFiles = "AllErEstEndingAdjectivesUniq.txt"
+
 
   var hashMapOfAllUniqAdjectivesInAgiga = Map("Long" -> "1")
 
+
+  var hashMapOfInflAdjToRootForm: Map[String, String] = Map()
 
 
   def ReadAllUniqAdjectivesToHashmap(): Unit = {
@@ -45,69 +49,16 @@ var completeAgigaFile="allAdjectivesFromAgigaButUniq.txt";
       //println("reaching here at 1263")
       var counterForHashmap = 0;
       for (line <- Source.fromFile(advInputFile).getLines()) {
-      //  println("reaching here at 184")
-	 hashMapOfAllUniqAdjectivesInAgiga += (line -> "1");
+        //  println("reaching here at 184")
+        hashMapOfAllUniqAdjectivesInAgiga += (line -> "1");
       }
     } catch {
-      case ex: Exception => println("Bummer, an exception happened.")
+      case ex: Exception => println("An exception happened. Not able to find the file")
     }
   }
 
 
-//  def processDocumentForAdverbs(doc: Document): ArrayBuffer[String] = {
-//    var arrayOflemmas = ArrayBuffer[String]()
-//    var lemma = "teststring";
-//    for (individualSentence <- doc.sentences) {
-//      for ((tag, wordCount) <- individualSentence.tags.get.zipWithIndex) {
-//        if (tag == "RB") {
-//          var advlemma = individualSentence.lemmas.get(wordCount)
-//          var numberOfTokens = individualSentence.words.length;
-//          // println("value of wordCount is :"+wordCount+" and the value of numberOf Tokens is:"+numberOfTokens)
-//
-//          //If total number of tokens == wordCount, it means the adverb is the last word of the sentence. quit/move onto next sentence.
-//          //wordcount:16 totaltokens:17
-//          if (wordCount < (numberOfTokens - 1)) {
-//            //proceed to find the beighboring adjective only if this new found adverb is part of shivade's list
-//            println("found an adverb. its value is: " + advlemma);
-//            //println( "Keys in colors : " + hashMapOfUniqAdjectives.keys )
-//            //println( "Values in colors : " + hashMapOfUniqAdjectives.values )
-//            if (hashMapOfUniqAdjectives.contains(advlemma)) {
-//              // println("Found adverb exists in shivade's list")
-//
-//
-//              //find the word next to this adverb i.e wordcount+1
-//
-//
-//              //we were hitting adverbs that end a sentence. So this wordcount+1 is hitting null. Doing a null check
-//              Option(individualSentence.tags.get(wordCount + 1)) match {
-//                case Some(i) => {
-//                  // println("given adjective word exists")
-//
-//                  {
-//                    var newTag = individualSentence.tags.get(wordCount + 1)
-//                    if (newTag.startsWith("JJ")) {
-//                      var adjlemma = individualSentence.lemmas.get(wordCount + 1);
-//                      println("found an adjective modified by adverb. The adverb is:" + advlemma + " and the adjective is:" + adjlemma);
-//                      //add this newly found lemma to the array of lemmas
-//                      arrayOflemmas += advlemma + " " + adjlemma;
-//                      // arrayOflemmas += adjlemma;
-//
-//                    }
-//                  }
-//                }
-//                case None => println(" There is no adjective next to the given adverb. Quitting/moving onto next line.")
-//              }
-//            } else {
-//              // println("Found adverb does not exist in shivade's list ")
-//            }
-//          }
-//        }
-//      }
-//    }
-//    return arrayOflemmas;
-//  }
-
-  def writeToFile():Unit= {
+  def writeToFile(): Unit = {
     //val outFile = new File(outputDirectoryPath, "adv_adj_" + fileName + ".txt")
     //      // make sure the file hasn't been already processed
     //      // useful when restarting
@@ -118,8 +69,15 @@ var completeAgigaFile="allAdjectivesFromAgigaButUniq.txt";
     //        bw.write(adverbs.mkString("\n "))
     //        bw.close()}
   }
+
   def printLine(): Unit = {
     println("finished writing adverbs to file")
+  }
+
+
+  def testForNull(x: Option[String]) = x match {
+    case Some(s) => s
+    case None => "value not found"
   }
 
   def readErRemovedFile(): Unit = {
@@ -135,63 +93,24 @@ var completeAgigaFile="allAdjectivesFromAgigaButUniq.txt";
       //var counterForHashmap = 0;
       for (line <- Source.fromFile(erRemovedInputFile).getLines()) {
         adjToCheck = line;
-        //println("reaching here at 8367. VALUE OF adjective is"+adjToCheck)
 
-        if  (hashMapOfAllUniqAdjectivesInAgiga.contains(line)) {
-          println("found that the given adjective:" + adjToCheck + " has its root form in the file. ")
+        //do the -er and -est removal in scala itself
+        var erEstRemovedForm = adjToCheck.replaceAll("er", "")
+        erEstRemovedForm = erEstRemovedForm.replaceAll("est", "")
+        // println("the root form of the given adjective:" + adjToCheck  + " is:"+erEstRemovedForm)
+        //if the er and est removed root form exists in the hashtable store it into another hashtable in the form "deepest->deep
+
+        if (hashMapOfAllUniqAdjectivesInAgiga.contains(erEstRemovedForm)) {
+          println("found that the given adjective:" + adjToCheck + " has its root form in the file. adding to hash map")
+          hashMapOfInflAdjToRootForm += (adjToCheck -> erEstRemovedForm);
         }
+
       }
     } catch {
-      case ex: Exception => println("No root form found for adjective:" + adjToCheck)
+      case ex: Exception => println("Exception occured:")
     }
+    println("value of hashmap is:" + hashMapOfInflAdjToRootForm);
   }
-
-
 }
 
 
-    //System.exit(1);
-    //run a random number generator inside an always true for loop- to pick files randomly and parse- right now all my threads are fighting for the same file
-
-    //for(1){
-      //
-//    while (true) {
-//
-//     var  randomFileNumber = rand.nextInt(1009)
-//    println("value of input folder is:"+baseDirectoryPath+"and the value of output directory is:"+outputDirectoryPath)
-//
-//      println("value of random number is:" + randomFileNumber)
-//     val individualFile = files(randomFileNumber)
-//     // for (individualFile <- files) {
-//      val fileName = individualFile.getName
-//      println("reaching here at 27")
-//      println("name of this file am parsing is:" + fileName)
-//      val outFile = new File(outputDirectoryPath, "adv_adj_" + fileName + ".txt")
-//      // make sure the file hasn't been already processed
-//      // useful when restarting
-//      println("reaching here at 2")
-//      if (!outFile.exists) {
-//        println("starting identification of adverbs in agigafile:" + fileName)
-//        println("reaching here at 3")
-//        println("the absolute path of the current document is: " + individualFile.getAbsolutePath)
-//        //for some reason agiga doesnt process the file i had hand made in my laptop. If your code has reached till here,
-//        //it means, its time to push it to jenny and test there. Somehow it works fine there.
-//        val doc = agiga.toDocument(individualFile.getAbsolutePath)
-//        println("finished finding adverbs in" + fileName)
-//        val adverbs = processDocumentForAdverbs(doc)
-//        println("reaching here at 4")
-//        //println("value of adverbs collected so far is "+adverbs.mkString("\n"))
-//        // val uniqAdj = adverbs.distinct
-//        //write to file
-//        println(" returned value of adverbs reaching here at 5")
-//        val bw = new BufferedWriter(new FileWriter(outFile))
-//        bw.write(adverbs.mkString("\n "))
-//        bw.close()
-//      }
-//    }
-//  }
-//}
-//
-//
-//
-//
